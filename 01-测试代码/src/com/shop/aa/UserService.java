9680b43afc45e7587bfa2723d47add885843e032
Administrator <xx@qq.com>
2019-06-26 18:55:58 +0800

    1111111111111111

--- 01-测试代码/src/com/shop/aa/UserService.java
+++ 01-测试代码/src/com/shop/aa/UserService.java
@@ -2,9 +2,6 @@ package com.shop.aa;
 
 public interface UserService {
    void addUser();
-   
-   void updateUser();
-   void cccc();
-   void test_brackn();
-   void aa();
+   // test
+   void cc();
 }
